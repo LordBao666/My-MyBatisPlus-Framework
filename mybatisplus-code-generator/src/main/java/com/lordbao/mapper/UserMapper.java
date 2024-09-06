3f22670687577be79005b8403384614884e287a0
LordBao666 <916900021@qq.com>
2024-09-06 12:03:21 +0800

    Finish mybatisplus code generator

---
+++ mybatisplus-code-generator/src/main/java/com/lordbao/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lordbao.mapper;
+
+import com.lordbao.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author ASUS
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-09-06 11:49:44
+* @Entity com.lordbao.pojo.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ mybatisplus-code-generator/src/main/java/com/lordbao/service/UserService.java
@@ -0,0 +1,13 @@
+package com.lordbao.service;
+
+import com.lordbao.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ASUS
+* @description 针对表【user】的数据库操作Service
+* @createDate 2024-09-06 11:49:44
+*/
+public interface UserService extends IService<User> {
+
+}
